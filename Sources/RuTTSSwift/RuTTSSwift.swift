2230f0d607cc54975af9963604dbaceea57d046c
Cyrmax <belousov.k.m@ya.ru>
2021-07-27 02:55:19 +0300

    Added ability to store callback inside the class

--- Sources/RuTTSSwift/RuTTSSwift.swift
+++ Sources/RuTTSSwift/RuTTSSwift.swift
@@ -18,14 +18,15 @@ public func CRuTTSCallBack(buffer: UnsafeMutableRawPointer?, bufferSize: Int, us
 public class RuTTSWrapper {
   private let waveBufferSize: Int = 4096
   var waveBuffer: UnsafeMutableRawPointer
+  var speechCallback: CRuTTSCallBackType
 
-  public init() {
+  public init(callback: @escaping CRuTTSCallBackType) {
     self.waveBuffer = UnsafeMutableRawPointer.allocate(byteCount: waveBufferSize, alignment: 1)
+    self.speechCallback = callback
   }
 
 public func makeSpeech(str: String) {
-  let callback: CRuTTSCallBackType = CRuTTSCallBack
-ru_tts_transfer(str, waveBuffer, waveBufferSize, callback, nil, 1, 1, 1, 1, 1)
+ru_tts_transfer(str, waveBuffer, waveBufferSize, speechCallback, nil, 1, 1, 1, 1, 1)
 }
 
 
